047b1c5807c35e2584360f72044c7803ff884b10
Gabriel <g.boisvert@student.isartdigital.com>
2022-04-27 13:13:00 -0400

    commit

--- Assets/Scripts/LDO/Enemy/MeleAI.cs
+++ Assets/Scripts/LDO/Enemy/MeleAI.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MeleAI : MonoBehaviour
 {
     private GameObject player;
+    public float speed = 2;
+    public float attackRange = 1;
     void Start()
     {
         
@@ -13,7 +15,26 @@ public class MeleAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (player != null)
+        {
+            Vector3 newPos = Vector3.MoveTowards(this.transform.position, player.transform.position, Time.deltaTime * this.speed);
+
+            if (Mathf.Abs(this.transform.position.x - player.transform.position.x) > this.attackRange)
+            {
+                newPos.y = this.transform.position.y;
+                this.transform.position = newPos;
+            }
+
+            if (Vector3.Distance(this.transform.position, player.transform.position) < 0.001f)
+            {
+                this.Attack();
+            }
+        }
+    }
+
+    void Attack()
+    { 
+          
     }
 
     private void OnTriggerEnter(Collider other)
