0651683b1d80819fd2fab6d9f68bd44d784e3502
perfectbrownies <gabriel.boisvert.dev@gmail.com>
2022-04-27 00:21:58 -0400

    Adding platform script

--- Assets/Scripts/Platform/MovingPlatform.cs
+++ Assets/Scripts/Platform/MovingPlatform.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class MovingPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] protected float duration = 2;
+    [SerializeField] protected Vector3 maxPos;
+    protected Vector3 minimum;
+    protected float startTime;
+    protected bool starting = true;
+    public Vector3 MaxPos { get => maxPos; set => maxPos = value; }
+    public void Start()
     {
-        
+        this.minimum = this.transform.position;
+        this.startTime = Time.time;
+    }
+    public void Update()
+    {
+        float t = (Time.time - this.startTime) / this.duration;
+
+        if (this.starting)
+            transform.position = Vector3.Lerp(this.minimum, this.minimum + this.MaxPos, t);
+        else
+            transform.position = Vector3.Lerp(this.minimum + this.MaxPos, this.minimum, t);
+
+        if (Time.time - this.startTime > this.duration)
+        {
+            this.starting = !this.starting;
+            this.startTime = Time.time;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player"))
+            collision.transform.SetParent(this.transform);
+    }
+
+    public void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            collision.transform.SetParent(null);
     }
 }
